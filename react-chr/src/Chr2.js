568a76081354610d0c6484312f2f9c4b80f961c8
강찬석 <ssimjunlke@naver.com>
2022-08-27 14:32:54 +0900

    modify

--- react-chr/src/Chr2.js
+++ react-chr/src/Chr2.js
@@ -11,12 +11,40 @@ function Chr2() {
         }
         return arr
     }
+    function test2_1(){
+      let arr2 = []
+      for (let i = 1; i < 25; i++){
+        for (let j = 1; j < 11; j++) {
+          arr2.push(<p>{i + '- ' + j}</p>)
+        }
+          
+      }
+      return arr2
+    }
+  
     return (
       <div className="Chr2">
-          <p>
-            {test2()}
-            </p>
+        <table style={{border:"5px"}}>
+          <thead>
+            <tr style={{background:"orange", color:"black"}}>
+              <th style={{width:"30%"}}>No</th>
+              <th style={{width:"70%"}}>Graph</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {test2_1().map((item) =>  */}
+            
+            <tr>
+            <td>{test2_1()}</td>
+            <td>{test2()}</td>
+            </tr>
+            {/* ) */}
+            {/* }        */}
+          </tbody>
           
+           
+          
+        </table>
       </div>
     );
   }

67fc8f5c4db2ee71798fa31a9d861dc79da6a573
강찬석 <ssimjunlke@naver.com>
2022-08-30 02:40:03 +0900

    main modify

--- react-chr/src/main.js
+++ react-chr/src/main.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function Main() {
     return (
       <div className="Main">
-        <h1 style={{margin:'10% 10% 10% 40%'}}>메인 페이지 입니다.</h1>
-        <ul style={{margin:'10% 10% 30% 40%'}}>
-            <Link to = '/chr2'><button style={{color:'white', background:'black', cursor:'pointer', fontSize:'20px',borderRadius:'5px', padding:'10px 60px', margin:'10px 0px'}}>chr</button></Link>
+        <h1 style={{margin:'10% 10% 10% 40%', alignItems:'center',justifyContent:'center',fontSize:'50px'}}>Select View</h1>
+        <ul style={{margin:'10% 0% 40% 35%'}}>
+            <Link to = '/chr2'><button style={{ alignItems:'center',justifyContent:'center',color:'white', background:'black', cursor:'pointer', fontSize:'20px',borderRadius:'5px', padding:'10px 60px', margin:'10px 0px'}}>Chromosome Position View</button></Link>
             <br/>
-            <Link to = '/cancer'><button style={{color:'white', background:'black', cursor:'pointer', fontSize:'20px',borderRadius:'5px', padding:'10px 60px', margin:'10px 0px'}}>cancer</button></Link>
+            <Link to = '/cancer'><button style={{ alignItems:'center',justifyContent:'center',color:'white', background:'black', cursor:'pointer', fontSize:'20px',borderRadius:'5px', padding:'10px 60px', margin:'10px 0px'}}>Cancer Screen View</button></Link>
         </ul>
           
       </div>
